9b27e3b0c15446037083032ed30cc3a1bcebc481
Carlin <kittyclubco@gmail.com>
2017-06-22 16:52:29 -0400

    Corrected bug with tablename
    corrected error where table was not referred to by name chosen in AS clause

--- GenWaybills.sql
+++ GenWaybills.sql
@@ -7,21 +7,20 @@ FROM ( -- what is *SQL goes here* getting?
     *SQL goes here*
 AS generatedtablename, otherTableToBeJoined
 WHERE cellfromtableA=IDfromtableB)
-
 */
 
-SELECT rawTbl.ID, rawTbl.Origin, rawTbl.OriginTown, rawTbl.OriginColor, rawTbl.OriginDistrict, rawTbl.destination, rawTbl.destinationTown, rawTbl.destinationColor, rawTbl.destinationDistrict, rawTbl.CarType, FLOOR(RAND()*(rawTbl.MaxQty-rawTbl.MinQty)+rawTbl.MinQty) AS Qty, rawTbl.Milepost  -- calculate random qty
+SELECT orders.ID, orders.Origin, orders.OriginTown, orders.OriginColor, orders.OriginDistrict, orders.destination, orders.destinationTown, orders.destinationColor, orders.destinationDistrict, orders.CarType, FLOOR(RAND()*(orders.MaxQty-orders.MinQty)+orders.MinQty) AS Qty, orders.Milepost  -- calculate random qty
 FROM ( -- add destination district color
     SELECT rawTbl.ID, rawTbl.Origin, rawTbl.OriginTown, rawTbl.OriginColor, rawTbl.OriginDistrict, rawTbl.destination, towns.Color AS destinationTown, rawTbl.destinationColor, rawTbl.destinationDistrict, rawTbl.CarType, rawTbl.Milepost, rawTbl.MinQty, rawTbl.MaxQty
-    FROM (-- add origin town colour
+    FROM ( -- add origin town colour
         SELECT rawTbl.ID, rawTbl.Origin, towns.Color AS OriginTown, rawTbl.OriginColor, rawTbl.OriginDistrict, rawTbl.destination, rawTbl.destinationTown, rawTbl.destinationColor, rawTbl.destinationDistrict, rawTbl.CarType, rawTbl.MinQty, rawTbl.MaxQty, rawTbl.Milepost
-        FROM(-- add destination district color
+        FROM( -- add destination district color
             SELECT  rawTbl.ID, rawTbl.Origin, rawTbl.OriginTown, rawTbl.OriginColor, rawTbl.OriginDistrict, rawTbl.destination, rawTbl.destinationTown, rawTbl.destinationColor, districts.Color AS destinationDistrict, rawTbl.CarType, rawTbl.MinQty, rawTbl.MaxQty, rawTbl.Milepost
-            FROM (-- add origin district color
+            FROM ( -- add origin district color
                 SELECT rawTbl.ID, rawTbl.Origin, rawTbl.OriginTown, rawTbl.OriginColor, districts.Color AS OriginDistrict, rawTbl.destination, rawTbl.destinationTown, rawTbl.destinationColor, rawTbl.destinationDistrict, rawTbl.CarType, rawTbl.MinQty, rawTbl.MaxQty, rawTbl.Milepost
-                FROM (-- add destination industries
+                FROM ( -- add destination industries
                     SELECT carorders.ID, carorders.Origin, carorders.OriginTown, carorders.OriginColor, carorders.OriginDistrict, industries.Name AS destination, industries.Town AS destinationTown, industries.IndustryColor AS destinationColor, industries.District AS destinationDistrict, carorders.CarType, carorders.MinQty, carorders.MaxQty, industries.Milepost
-                    FROM (-- gen table with origin industries filled in
+                    FROM ( -- gen table with origin industries filled in
                         SELECT carordersrandom.ID, industries.Name AS Origin, industries.Town AS OriginTown, industries.IndustryColor AS OriginColor, industries.District AS OriginDistrict, carordersrandom.Destination, carordersrandom.CarType, carordersrandom.MinQty, carordersrandom.MaxQty
                         FROM industries, carordersrandom
                         WHERE industries.ID=carordersrandom.Origin)
@@ -35,5 +34,5 @@ FROM ( -- add destination district color
         WHERE towns.ID=rawTbl.originTown)
     AS rawTbl, towns
     WHERE towns.ID=rawTbl.destinationTown)
-AS currentcarorders
+AS orders
 ORDER BY Milepost; -- List by the box that  the origen waybill is in. Why we called this field milepost is beyond me.
